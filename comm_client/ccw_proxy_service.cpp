0f1550dfae74eab47471be48f6cbf8878ce9219f
Ran Proshan <ran.proshan@gmail.com>
2018-05-30 10:43:45 +0300

    ccw dev work

--- comm_client/ccw_proxy_service.cpp
+++ comm_client/ccw_proxy_service.cpp
@@ -1,4 +1,10 @@
 
+#include <stdlib.h>
+
+#include <string>
+
+#include "comm_client_cb_api.h"
+
 #include "ccw_proxy_service.h"
 
 
--- comm_client/ccw_proxy_service.h
+++ comm_client/ccw_proxy_service.h
@@ -1,7 +1,33 @@
 
 #pragma once
 
-class ccw_proxy_service
+class ccw_proxy_service : public comm_client_cb_api
 {
 public:
+	ccw_proxy_service(const char * log_category);
+	virtual ~ccw_proxy_service();
+
+	typedef struct __client
+	{
+		unsigned int id;
+		unsigned int count;
+		std::string conf_file;
+
+		__client()
+		: id((unsigned int)-1), count((unsigned int)-1)
+		{}
+	}client_t;
+
+	typedef struct __service
+	{
+		__service()
+		{}
+	}service_t;
+
+	//comm_client_cb_api
+	virtual void on_comm_up_with_party(const unsigned int party_id);
+	virtual void on_comm_down_with_party(const unsigned int party_id);
+	virtual void on_comm_message(const unsigned int src_id, const unsigned char * msg, const size_t size);
+
+	int serve(const service_t & a_svc, const client_t & a_clnt);
 };
