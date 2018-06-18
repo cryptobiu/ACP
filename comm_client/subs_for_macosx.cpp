bb9664730acb2ac0ea53006cd9f07f89ae5cd7c0
Ran Proshan <ran.proshan@gmail.com>
2018-06-18 10:56:15 +0300

    typo fix

--- comm_client/subs_for_macosx.cpp
+++ comm_client/subs_for_macosx.cpp
@@ -19,7 +19,7 @@ int pthread_mutex_timedlock(pthread_mutex_t * lock, struct timespec * abs_to)
 	return result;
 }
 
-ssize_t splice(int fd_in, loff_t *off_in, int fd_out, loff_t *off_out, size_t len, unsigned int flags)
+ssize_t splice(int fd_in, off_t *off_in, int fd_out, off_t *off_out, size_t len, unsigned int flags)
 {
 	return sendfile(fd_out, fd_in, off_in, len);
 }
--- comm_client/subs_for_macosx.h
+++ comm_client/subs_for_macosx.h
@@ -3,7 +3,7 @@
 
 int pthread_mutex_timedlock(pthread_mutex_t * lock, struct timespec * abs_to);
 
-ssize_t splice(int fd_in, loff_t *off_in, int fd_out, loff_t *off_out, size_t len, unsigned int flags);
+ssize_t splice(int fd_in, off_t * off_in, int fd_out, off_t * off_out, size_t len, unsigned int flags);
 
 int pthread_timedjoin_np(pthread_t thread, void **retval, const struct timespec *abstime);
 
