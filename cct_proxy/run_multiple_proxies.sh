28c0ac6d0762869c03e23cb815666017c0f14d49
liorbiu <liork.cryptobiu@gmail.com>
2018-06-25 11:02:10 +0300

    Add number of peers variable

--- cct_proxy/run_multiple_proxies.sh
+++ cct_proxy/run_multiple_proxies.sh
@@ -3,5 +3,5 @@
 for i in `seq 0 1 ${1}`;
 do
     port_number=$((9000+${i}))
-    ./cct_proxy -i ${i} -c $((${1}+1)) -f parties.conf -l 700 -p ${port_number} &
+    ./cct_proxy -i ${i} -c ${2} -f parties.conf -l 700 -p ${port_number} &
 done
